8abab1a12287341f817c31d2494f16f49b49e3e6
exfinder <exfinder@gmail.com>
2025-04-10 20:18:55 +0300

    private subnets

--- tf/main.tf
+++ tf/main.tf
@@ -2,3 +2,14 @@ resource "aws_vpc" "main" {
   cidr_block         = "10.0.0.0/16"
   enable_dns_support = true
 }
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index + 1}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+}
